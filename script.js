1f871da5680a6b07701404299b686970de287af9
Alex Agaev <alex34agaev@gmail.com>
2020-08-18 18:33:54 +0400

    new

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let burger = document.querySelector('.burger_menu');
 let menu = document.querySelector('.intro_menu');
+let title = document.querySelector('.intro_title')
 
 burger.addEventListener('click', function () {
     menu.classList.toggle('burger_position');
+    title.classList.toggle('burger_margin');
 })
